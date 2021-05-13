a32ca6cd1498bd9522778be889ddeb7b7a48f135
vidalmaxime <mvidal@student.ethz.ch>
2021-05-13 16:40:20 +0200

    files

--- frontend/src/views/home/home.js
+++ frontend/src/views/home/home.js
@@ -1,25 +1,46 @@
 import navbar from "../../components/Navbar.vue";
-import { ref, onBeforeMount } from 'vue';
+import {ref, onBeforeMount} from 'vue';
 import firebase from 'firebase';
+
+
 export default {
     name: 'home',
-    setup () {
+    setup() {
 
         const name = ref("");
-        onBeforeMount(()=> {
+        onBeforeMount(() => {
             const user = firebase.auth().currentUser;
             console.log(user);
             if (user) {
-                name.value= user.email.split("@")[0];
+                name.value = user.email.split("@")[0];
             }
+
+            // var storage    = firebase.storage();
+            // var storageRef = storage.ref();
+            // var listRef = storageRef.child('images/');
+            //
+            // // Find all the prefixes and items.
+            // listRef.listAll()
+            //     .then((res) => {
+            //         res.items.forEach((itemRef) => {
+            //             console.log(itemRef.getDownloadURL());
+            //             console.log("itemref", itemRef);
+            //             // All the items under listRef.
+            //         });
+            //     }).catch((error) => {
+            //     // Uh-oh, an error occurred!
+            // });
+
+
         });
+
 
         const Logout = () => {
             firebase
                 .auth()
                 .signOut()
                 .then(() => console.log("Signed out"))
-                .catch(err=> alert(err.message));
+                .catch(err => alert(err.message));
         }
 
         return {
@@ -27,16 +48,110 @@ export default {
             Logout
         }
     },
+
+    // beforeMount() {
+    //         var storage = firebase.storage();
+    //         var storageRef = storage.ref();
+    //         let promises = this.images.map((image, i) => {
+    //             console.log("image",image);
+    //             return storageRef.child(`images/${image}`).getDownloadURL();
+    //         });
+    //         Promise.all(promises).then((downloadURLs) => {
+    //             console.log("download",downloadURLs);
+    //             this.image_one=downloadURLs[0]
+    //         });
+    //
+    //
+    // },
+
     data: function () {
         return {
-            confidence:15,
+            value: 15,
+            image_one: "",
+            image_two:"",
+            images: ["598A0068_Rocky.JPG", "598A0078_Holden.JPG", "598A0080_Fisher.JPG", "598A0093_Waterfalls.JPG", "598A0157_Aardvark.JPG", "598A0171_Panda.JPG"],
+            started: false,
+            index:2,
+            bear_name: "",
         };
     },
+
     methods: {
+        // mounted() {
+        //     var storage = firebase.storage();
+        //     var storageRef = storage.ref();
+        //     var listRef = storageRef.child('images/');
+        //     storageRef.child('images/'+this.images[0]).getDownloadURL().then(function (url) {
+        //         var image_one = url;
+        //         console.log("imageone", image_one);
+        //         this.image_one = image_one;
+        //         document.getElementById("imagepairone").src = string(image_one);
+        //     }).catch(function (error) {
+        //     });
+        //     storageRef.child('images/'+this.images[1]).getDownloadURL().then(function (url) {
+        //         var image_two = url;
+        //         console.log("imagetwo", image_two);
+        //         this.image_two = image_two;
+        //     }).catch(function (error) {
+        //     });
+        //     console.log("MOUNTED");
+        //
+        // },
+
+        nextImage() {
+            // var storage = firebase.storage();
+            // var storageRef = storage.ref();
+            // console.log(this.image_one)
+            // var listRef = storageRef.child('images/');
+            // storageRef.child('images/'+this.images[0]).getDownloadURL().then(function (url) {
+            //     var image_one = url;
+            //     this.image_one = url; //image_one;
+            //     console.log("imageone", this.image_one);
+            //     document.getElementById("imagepairone").src = image_one;
+            // }).catch(function (error) {
+            // });
+            // storageRef.child('images/'+this.images[1]).getDownloadURL().then(function (url) {
+            //     var image_two = url;
+            //     console.log("imagetwo", image_two);
+            //     this.image_two = image_two;
+            // }).catch(function (error) {
+            // });
+
+            var storage = firebase.storage();
+            var storageRef = storage.ref();
+            // do this on mounted then store the list on client and pick from there? so only 1 request
+            let promises = this.images.map((image, i) => {
+                console.log("image",image);
+                return storageRef.child(`images/${image}`).getDownloadURL();
+            });
+            Promise.all(promises).then((downloadURLs) => {
+                console.log("download",downloadURLs);
+                this.image_one=downloadURLs[this.index]
+                this.image_two=downloadURLs[this.index+1]
+            });
+            this.index += 2;
+            this.value = 15;
+            this.bear_name = "";
+
+        },
+
+        startExperiment() {
+            this.started=true;
+            var storage = firebase.storage();
+            var storageRef = storage.ref();
+            let promises = this.images.map((image, i) => {
+                console.log("image",image);
+                return storageRef.child(`images/${image}`).getDownloadURL();
+            });
+            Promise.all(promises).then((downloadURLs) => {
+                console.log("download",downloadURLs);
+                this.image_one=downloadURLs[0]
+                this.image_two=downloadURLs[1]
+            });
+        }
 
     },
-    computed: {
-    },
+    computed: {},
     components: {
         navbar
     },
--- frontend/src/views/home/home.vue
+++ frontend/src/views/home/home.vue
@@ -2,13 +2,15 @@
   <navbar></navbar>
   <br>
   <div class="home">
+
+    <div v-if="started">
     <div class="container mx-auto flex justify-center items-center ">
-      <img class="h-72 w-auto px-4" src="../../assets/bearface.jpg"
-           alt="Bear">
+      <img class="h-72 w-auto px-4" :src="image_one"
+           alt="Bear1" >
 
 
-    <img class="h-72 w-auto px-4" src="../../assets/bearface.jpg"
-         alt="Bear">
+    <img class="h-72 w-auto px-4" :src="image_two"
+         alt="Bear2">
     </div>
   <br>
 
@@ -26,7 +28,7 @@
     <br>
     <div>
       <label class="block text-sm font-medium text-gray-700">Confidence</label>
-    <input class="rounded-lg overflow-hidden appearance-none bg-gray-300 h-3 w-128" type="range" min="1" max="100" step="1" value=this.confidence />
+    <input class="rounded-lg overflow-hidden appearance-none bg-gray-300 h-3 w-128" type="range" min="1" max="100" step="1" v-model="value"/>
     </div>
     <br>
       <div>
@@ -34,12 +36,17 @@
       <div class="mt-1 relative rounded-md shadow-sm">
         <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
         </div>
-        <input type="text" name="identity" id="identity" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="Aardvark" />
+        <input v-model="bear_name" type="text" name="identity" id="identity" class="focus:ring-green-500 focus:border-green-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="Aardvark" />
       </div>
+        <button @click="nextImage()" class="mx-auto my-5 flex justify-center items-center bg-green-500 hover:bg-green-400 text-gray-800 font-bold py-2 px-4 rounded focus:bg-green-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-300 focus:ring-white"> Next </button>
     </div>
 
   </div>
 
+  </div>
+    <div v-else>
+      <button @click="startExperiment()" class=" mx-auto my-5 flex justify-center items-center bg-green-500 hover:bg-green-400 text-gray-800 font-bold py-2 px-4 rounded focus:bg-green-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-300 focus:ring-white"> Start</button>
+    </div>
   </div>
 </template>
 
