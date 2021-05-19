dcd1be3afd43d507d1c54e93729fd9658587d6b1
vidalmaxime <mvidal@student.ethz.ch>
2021-05-19 17:26:55 +0200

    fixed authentification?

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,15 @@
 import {createWebHistory, createRouter} from "vue-router";
 import home from "../views/home/home.vue";
+import firebase from "firebase";
 
 const routes = [
     {
         path: "/",
         name: "home",
         component: home,
+        meta: {
+            requiresAuth: true
+        }
     },
     {
         path: '/login',
@@ -17,11 +21,32 @@ const routes = [
         name: 'Register',
         component: () => import('../components/Register.vue')
     },
+    // {
+    //     path:'*',
+    //     redirect:'/'
+    // }
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
 });
+
+router.beforeEach((to, from, next) => {
+
+    const currentUser = firebase.auth().currentUser;
+    const requireAuth = to.matched.some(record => record.meta.requiresAuth);
+
+    if(requireAuth && !currentUser){
+        next({ name: 'Login'});
+    }
+    else if(!requireAuth && currentUser){
+        next({ name: 'home'});
+    }
+    else {
+        next();
+    }
+});
+
 
 export default router;
